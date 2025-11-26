b4bb03947fa34542ff1ac6f2b02c0475f8d8d855
Cyberhawks706 <robotics@arrowheadschools.org>
2025-11-26 13:57:45 -0600

    vision
    Co-authored-by: NitinAyyappan <NitinAyyappan@users.noreply.github.com>
    Co-authored-by: ghRevelle <ghRevelle@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -19,73 +20,84 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.LimelightHelpers;
+import frc.robot.subsystems.VisionSubsystem;
 
 public class RobotContainer {
-    private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+	private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+	private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
-    /* Setting up bindings for necessary control of the swerve drive platform */
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+	/* Setting up bindings for necessary control of the swerve drive platform */
+	private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+			.withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+			.withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+	private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+	private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-    private final Telemetry logger = new Telemetry(MaxSpeed);
+	private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    private final CommandXboxController joystick = new CommandXboxController(0);
+	private final CommandXboxController joystick = new CommandXboxController(0);
 
-    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+	public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
-    public RobotContainer() {
-        configureBindings();
-        
-        // Set the logger to log to the first flashdrive plugged in
-        SignalLogger.setPath("/media/sda1/");
+	public final VisionSubsystem visionSubsystem = new VisionSubsystem(drivetrain.getPose().getRotation()::getDegrees);
 
-        // Explicitly start the logger
-        SignalLogger.start();
 
-    }
+	public RobotContainer() {
+		configureBindings();
+		
+		// Set the logger to log to the first flashdrive plugged in
+		SignalLogger.setPath("/media/sda1/");
 
-    private void configureBindings() {
-        // Note that X is defined as forward according to WPILib convention,
-        // and Y is defined as to the left according to WPILib convention.
-        drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-            )
-        );
+		// Explicitly start the logger
+		SignalLogger.start();
+	}
 
-        // Idle while the robot is disabled. This ensures the configured
-        // neutral mode is applied to the drive motors while disabled.
-        final var idle = new SwerveRequest.Idle();
-        RobotModeTriggers.disabled().whileTrue(
-            drivetrain.applyRequest(() -> idle).ignoringDisable(true)
-        );
+	private void configureBindings() {
+		// Note that X is defined as forward according to WPILib convention,
+		// and Y is defined as to the left according to WPILib convention.
+		drivetrain.setDefaultCommand(
+			// Drivetrain will execute this command periodically
+			drivetrain.applyRequest(() ->
+				drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+					.withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+					.withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+			)
+		);
 
-        joystick.x().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.y().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+		// Idle while the robot is disabled. This ensures the configured
+		// neutral mode is applied to the drive motors while disabled.
+		final var idle = new SwerveRequest.Idle();
+		RobotModeTriggers.disabled().whileTrue(
+			drivetrain.applyRequest(() -> idle).ignoringDisable(true)
+		);
 
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+		joystick.x().whileTrue(drivetrain.applyRequest(() -> brake));
+		joystick.y().whileTrue(drivetrain.applyRequest(() ->
+			point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+		));
 
-        // reset the field-centric heading on b button press
-        joystick.b().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+		// Run SysId routines when holding back/start and X/Y.
+		// Note that each routine should be run exactly once in a single log.
+		joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+		joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+		joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+		joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
-        drivetrain.registerTelemetry(logger::telemeterize);
-    }
+		// reset the field-centric heading on b button press
+		joystick.b().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
-    public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
-    }
+		drivetrain.registerTelemetry(logger::telemeterize);
+	}
+
+	public Command getAutonomousCommand() {
+		return Commands.print("No autonomous command configured");
+	}
+
+	public void updateVisionPose() {
+		LimelightHelpers.PoseEstimate limelightMeasurement = visionSubsystem.getPoseEstimate();
+		drivetrain.addVisionMeasurement(limelightMeasurement.pose, limelightMeasurement.timestampSeconds);
+	}
+
+	
 }
---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,113 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.cscore.HttpCamera;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.LimelightHelpers;
+import frc.robot.subsystems.LimelightHelpers.PoseEstimate;
+import java.util.function.DoubleSupplier;
+
+
+public class VisionSubsystem extends SubsystemBase {
+  private ShuffleboardTab tab;
+  private double lastDistance;
+  private DoubleSupplier rotationSupplier;
+
+  private HttpCamera LLFeed;
+
+
+  public VisionSubsystem(DoubleSupplier rotationSupplier) {
+    setUpShuffleboard();
+    this.rotationSupplier = rotationSupplier;
+  }
+
+  private String getLimelightName() {
+    return "limelight";
+  }
+
+  public double getTX() {
+    return LimelightHelpers.getTX(getLimelightName());
+  }
+
+  public double getTY() {
+    return LimelightHelpers.getTY(getLimelightName());
+  }
+
+  public Pose2d getPose2dFromLimelight() {
+    return LimelightHelpers.getBotPose2d(getLimelightName());
+  }
+
+  public Pose2d getMegaTag2Pose2dFromLimelight() {
+    return LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(getLimelightName()).pose;
+  }
+
+  public Pose3d getPose3dTargetSpaceFromLimelight() {
+    return (LimelightHelpers.getBotPose3d_TargetSpace(getLimelightName()));
+  }
+
+  public PoseEstimate getPoseEstimate() {
+    return LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(getLimelightName());
+  }
+
+  public double getLatencyCapture() {
+    return LimelightHelpers.getLatency_Capture(getLimelightName());
+  }
+
+  public double getLatencyPipeline() {
+    return LimelightHelpers.getLatency_Pipeline(getLimelightName());
+  }
+
+  public boolean hasTarget() {
+    return LimelightHelpers.getTV(getLimelightName());
+  }
+
+  public double getAprilTagId() {
+    return LimelightHelpers.getFiducialID(getLimelightName());
+  }
+
+//   public void setPipeline(String pipeline) {
+//     LimelightHelpers.setPipelineIndex(getLimelightName(), pipeline.ordinal());
+//   }
+
+
+  public double getDistanceToTarget() {
+    if (!hasTarget()) {
+      return lastDistance;
+    }
+    double cameraHeight = 22;
+    double targetHeight = 56.375;
+    double heightDiff = targetHeight - cameraHeight;
+    double cameraAngle = 23;
+    double theta = Math.toRadians(cameraAngle + getTY());
+    lastDistance = heightDiff / Math.tan(theta);
+    return lastDistance;
+  }
+
+  @Override
+  public void periodic() {
+    LimelightHelpers.SetRobotOrientation(
+        getLimelightName(), rotationSupplier.getAsDouble(), 0, 0, 0, 0, 0);
+  }
+
+  private void setUpShuffleboard() {
+    tab = Shuffleboard.getTab("VisionApriltag");
+
+    LLFeed =
+        new HttpCamera(
+            getLimelightName(),
+            "http://10.7.6.11:5800/stream.mjpg"); // TODO check if this IP is correct
+    tab.add("Limelight Feed", LLFeed).withWidget("Camera Stream").withPosition(0, 0).withSize(4, 4);
+
+    tab.addDouble("Tx", () -> this.getTX());
+    tab.addDouble("Ty", () -> this.getTY());
+    tab.addDouble("Distance", () -> this.getDistanceToTarget());
+    tab.addDouble("Latency Capture", () -> this.getLatencyCapture());
+    tab.addDouble("Latency Pipeline", () -> this.getLatencyPipeline());
+    tab.addBoolean("Has Target", () -> this.hasTarget());
+
+    tab.addDouble("April Tag", () -> this.getAprilTagId());
+  }
+}
